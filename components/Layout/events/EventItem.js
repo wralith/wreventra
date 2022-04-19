b8728e30ba5580ba3f7a9cd452ba4487d11f6783
Wralith <wralithdev@gmail.com>
2022-04-19 20:30:16 +0300

    🎨 Add style to event cards

--- components/Layout/events/EventItem.js
+++ components/Layout/events/EventItem.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function EventItem(props) {
   return (
-    <li>
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
-      <img src={props.image} alt={`${props.title}-img`}/>
+    <li className="flex border hover:border-red-500 border-transparent w-[90%] md:w-[45%] xl:w-[30%] hover:scale-105 transition-all flex-col items-center gap-5 rounded-md bg-gray-800/50 p-5 text-gray-100">
+      <h2 className="mt-3 p-3">{props.title}</h2>
+      <img src={props.image} alt={`${props.title}-img`} />
+      <p className="mt-12 mb-6 p-2 ">{props.description}</p>
     </li>
-  )
+  );
 }
 
-export default EventItem
+export default EventItem;
--- components/Layout/events/EventList.js
+++ components/Layout/events/EventList.js
@@ -1,9 +1,20 @@
 import EventItem from "./EventItem";
 
 function EventList(props) {
-  return props.events.map((event) => (
-    <EventItem key={event.id} id={event.id} title={event.title} description={event.description} image={event.image} />
-  ));
+  return (
+    <ul className="flex flex-col items-center justify-center gap-12 m-2 p-5 md:flex-row md:flex-[2_2_0%] flex-wrap">
+      {" "}
+      {props.events.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          description={event.description}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default EventList;
